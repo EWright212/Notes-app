e5244c5ab913665fe04f77ac50e02fc26da293c1
Student <emilywright212@gmail.com>
2020-03-03 15:35:24 +0000

    Method add note added to Note List

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -1,6 +1,13 @@
 describe('Note list', () => {
-  it('has can take a note', () => {
-    var noteList = new NoteList("note 1");
+  it('can take a note and return it', () => {
+    var noteList = new NoteList();
+    noteList.addNote("note 1")
     expect(noteList.getNotes()).toBe("note 1")
   })
+  // it('can take two notes and return them', () => {
+  //   var noteList = new NoteList("note 1");
+  //   noteList.addNote("note 1")
+  //   noteList.addNote("note 2")
+  //   expect(noteList.getNotes()).toBe("note 1, note 2")
+  // })
 })
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,7 +1,11 @@
-var NoteList = function(newNote) {
-  this.list = [newNote];
+var NoteList = function() {
+  this.list = ["note 1"];
 }
 
 NoteList.prototype.getNotes = function() {
   return this.list[0]
+}
+
+NoteList.prototype.addNote = function(newNote) {
+  this.list.push(newNote)
 }
